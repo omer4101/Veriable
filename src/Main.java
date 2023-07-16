6590a63a1de3dd75aaacdf62886811cc7131948d
omerbariskayaalp <omerbariskayaalp@gmail.com>
2023-07-16 16:32:36 +0300

    Veriables

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[]args)
+
+    {
+        int number=41;
+        String message="Hello";
+        double number1=1.3461;
+        float number2=999999999;
+        char character='A';
+        System.out.println(message);
+    }
+}
+
